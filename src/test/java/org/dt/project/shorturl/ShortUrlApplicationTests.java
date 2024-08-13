65b608e0b5509b5b7bb5685b98fe19c30c2afcd4
Daniel <tatardaniel10@gmail.com>
2024-08-13 15:42:45 +0300

    initial commit

---
+++ src/test/java/org/dt/project/shorturl/ShortUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dt.project.shorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
